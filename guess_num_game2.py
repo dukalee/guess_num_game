ab797fb73d68d578ef696f1902ecf812fd89724a
Michael Lee <dukalee@naver.com>
2018-02-19 16:35:01 +0900

    Added instructions and retry input

--- guess_num_game2.py
+++ guess_num_game2.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
 
-"""
-숫자 맞추기 IQ 게임
-Guess the 4-Digit number
-
-PROGRAM NAME: guess_num_game2.py
-VERSION: 1.1
-CREATOR: dukalee
-DATE: February 17, 2018 (last edited)
-======================
-
-It's a game to guess a 4 digit number with the smallest number of tries as possible. 
-when you enter a 4-digit number, you can get a hint according to your guess. 
-1. How many numbers are correct. 
-2. How many numbers are correct and in the right position.
-
-4자릿수의 비밀 숫자를 최소한의 횟수로 맞추는 게임입니다. 
-4자리의 숫자를 입력하면 비밀 숫자에 대한 힌트를 얻을 수 있습니다. 
-1. 자리 '와' 숫자가 맞았는지, 
-2. 숫자가 맞았는지 
-
---------------------------------------------
-Guess the Number!4567
-Number of Correct Numbers: 1
-Number of Correct Numbers at correct Places: 2
---------------------------------------------
-
-라는 힌트를 얻을 수 있습니다. 
-
-
-VERSION
----------------------
-1.1 : changes made so that a 4-digit number containing duplicate numbers will not be created.
-
-"""
-
 import random 
+
+def instructions(): 
+
+    print("""
+
+             ----GUESS THE NUMBER!---
+
+You will be given a secret 4-digit number to guess. 
+Every time you enter a 4 digit number, if it is wrong, 
+you will be given hints to help you deduce the secret 
+number. Your job is to guess it with the smallest attempts
+as possible. 
+
+                    GOOD LUCK!
+        """)
+
 
 def create_secret_num(): 
 
@@ -54,37 +35,39 @@ def game():
     function to play the game 
     """
 
-    secret_num = create_secret_num()                                               #choose random number between 1000 and 9999
-    print(secret_num)
-    print(secret_num[1])
+    instructions()
+
+    secret_num = create_secret_num()                                              #choose random number between 1000 and 9999
     game = True
     tries = 0 
 
     while game:
 
-        guess = input("Guess the Number!")
+        print("Guess the Number!")
+        guess = input(">>> ")
 
         if guess.isdigit(): 
             if len(guess) == 4: 
                 tries += 1
 
-                if guess == secret_num:                                                                 #if guess is correct
+                if guess == secret_num:                                            #if guess is correct
                     print("You got it! the number is: {}".format(secret_num))
-                    print("tries: {}".format(tries))                                                    #show how many tries the player took to guess the number right
+                    print("tries: {}".format(tries))                               #show how many tries the player took to guess the number right
                     game = False
+                    retry()
                 else:
-                    c_num = 0                                                                           #number of correct numbers of a guess
-                    c_placeandnum = 0                                                                   #number of correct numbers in right places of a guess
+                    c_num = 0                                                      #number of correct numbers of a guess
+                    c_placeandnum = 0                                              #number of correct numbers in right places of a guess
                     i = 0 
                     num = ''
 
                     while i < len(guess):
-                        if guess[i] == secret_num[i]:                                                   #if the indexes of two numbers are same (at the same position)
+                        if guess[i] == secret_num[i]:                              #if the indexes of two numbers are same (at the same position)
                             c_placeandnum += 1
-                        else: 
-                            if guess[i] in secret_num:                                                  #if the number is in the secret number and not in 
+                        elif guess[i] in secret_num:                               #if the number of the guess is in the secret number but not at the same position
+                            if guess[i] not in num:                                #if that number is not already recorded
+                                num += guess[i]                                              
                                 c_num += 1
-                                num += guess[i]
                         i+=1
                     print("Number of Correct Numbers: {}".format(c_num))
                     print("Number of Correct Numbers at correct Places: {}".format(c_placeandnum))
@@ -92,6 +75,36 @@ def game():
                 print("Enter a 4-digit number!")
         else: 
             print("Enter a Number!")
+
+
+def retry(): 
+
+    print("""
+
+        Do you wish to play again? (Y/N)
+
+        """)
+
+    ans = input(">>> ")
+
+    if ans.lower() == 'y': 
+        game == True
+        game() 
+        
+    elif ans.lower() =='n': 
+        print("""
+
+            Thank you for playing!
+
+            """)
+    else: 
+        print("""
+
+            Please enter a valid input (Y/N)
+
+            """)
+
+
 
 
 if __name__ == "__main__":
